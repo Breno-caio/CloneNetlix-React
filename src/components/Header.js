c435c3dc40cc8cdb18f92876b927522e67a939cd
Breno Rocha <brenocaio.rocha@gmail.com>
2020-12-28 14:02:48 +0000

    App

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+export default ({black}) => {
+    return (
+
+       <header className={black ? 'black' : ''}>
+           <div className="header--logo">
+               <a href="/">
+                   <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"/>
+               </a>
+           </div>
+           <div className="header--user">
+               <a href="/">
+                   <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"/>
+               </a>
+           </div>
+       </header>
+
+    );
+}
